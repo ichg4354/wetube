9c7236526ad9d008a370f48f24d46c3a6f7349a5
ichg4354 <60804817+ichg4354@users.noreply.github.com>
2020-05-12 20:24:19 +0900

    not important

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
+const PORTS = 4000
 
 function handleListening() {
-    console.log('listening')
+    console.log(`listening to http://localhost:${PORTS}`)
+}
+function handleHome(req,res) {
+    // console.log(req)
+    res.send('hi from home')
 }
 
-app.listen(4000,handleListening)
+function handleProfile(req, res) {
+    // console.log(req)
+    res.send('hi from profile')
+}
+
+app.get('/', handleHome)
+
+app.get('/profile',handleProfile)
+
+app.listen(PORTS, handleListening)
+
