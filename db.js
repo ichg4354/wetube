35335b58fe81e1cd117b6ffb49d2096dde46d005
ichg4354 <60804817+ichg4354@users.noreply.github.com>
2020-07-22 23:55:15 +0900

    finish connection to mongo atlas

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_ATLAS_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology:true
-})
+const db = mongoose.connection;
 
-const db = mongoose.connection
+const handleOpen = () => console.log("✅ connected to DB");
+const handleError = (error) =>
+  console.log(`❌ error on DB connection ${error}`);
 
-const handleOpen = () => console.log('✅ connected to DB')
-const handleError = (error) => console.log(`❌ error on DB connection ${error}`)
-
-db.once('open', handleOpen)
-db.on('error', handleError)
+db.once("open", handleOpen);
+db.on("error", handleError);
